9aab2aeee73228f01ac5c8e80bf37105ca2cbf0a
Stepan358 <59833944+Stepan358@users.noreply.github.com>
2020-01-15 17:55:15 +0300

    решил задачу
    решил задачу if 5

---
+++ Stepan-if5/Stepan-if5/Stepan-if5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int num1, num2, num3, otr = 0, pol = 0;
+
+	cin >> num1 >> num2 >> num3;
+	if (num1 < 0)
+	{
+		otr++;
+	}
+	else
+	{
+		pol++;
+	}
+	if (num2 < 0)
+	{
+		otr++;
+	}
+	else
+	{
+		pol++;
+	}
+	if (num3 < 0)
+	{
+		otr++;
+	}
+	else
+	{
+		pol++;
+	}
+	cout << "отрицательных чисел:" << otr << endl << "положительных чисел:" << pol;
+	return 0;
+}
