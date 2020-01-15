bf2065f3600f6a97203249d536b0d007136840a1
Stepan358 <59833944+Stepan358@users.noreply.github.com>
2020-01-15 18:19:21 +0300

    решил задачу
    решил задачу begin 5

---
+++ Stepan-begin5/Stepan-begin5/Stepan-begin5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	int a;
+	cin >> a;
+	cout << "объём куба: " << a * a * a << endl << "площадь поверхности куба: " << a * a * 6;
+ 
+	return 0;
+}
