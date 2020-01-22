a29d3f266b0074ed4594f3ab8e2775a61229586d
Stepan <59833944+Stepan358@users.noreply.github.com>
2020-01-22 16:13:10 +0300

    Решил задачу.
    Решил задачу Array 2.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= N; i *= 2)
+            {
+                numbers.Add(i);
+            }
+            foreach (int value in numbers)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
