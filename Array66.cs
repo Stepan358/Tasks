b395d20ed23a666af89dd6938f3cf27836077874
Stepan358 <59833944+Stepan358@users.noreply.github.com>
2020-01-26 22:25:10 +0300

    Решил задачу.
    Решил задачу Array 66.

---
+++ Array66.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int N = Convert.ToInt32(Console.ReadLine());
+            int ch = 0;
+            int ch1 = 0;
+            bool ch1b = false;
+            for (int i = 0; i < N; i++)
+            {
+                numbers.Add(i);
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                if(numbers[i] % 2 == 0 && ch == 0 )
+                {
+                    ch1 = numbers[i];
+                    ch1b = true;
+                }
+                if(numbers[i] % 2 == 0 && ch1b == true)
+                {
+                    ch = numbers[i] * ch1;
+                    numbers[i] = ch;
+                }
+                
+
+            }
+            foreach (int value in numbers)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
