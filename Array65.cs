5be50265df6fdb813c66994779e7df04dd61bbb7
Stepan358 <59833944+Stepan358@users.noreply.github.com>
2020-01-26 21:36:55 +0300

    Решил задачу.
    Решил задачу Array 65/

---
+++ Array65.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int N = Convert.ToInt32(Console.ReadLine());
+            for ( int i=0; i < N; i++)
+            {
+                numbers.Add(i);
+                numbers[i] = Convert.ToInt32(Console.ReadLine()) + i;
+            }
+            foreach (int value in numbers)
+            {
+                Console.WriteLine(value + 1);
+            }
+        }
+    }
+}
