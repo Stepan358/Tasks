c9b694d6c3dcafba21187a063230117899652c6d
Stepan358 <59833944+Stepan358@users.noreply.github.com>
2020-01-26 20:11:04 +0300

    Create Array52.cs

---
+++ Array52.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            List<int> numbers2 = new List<int>();
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= N; i++)
+            {
+                numbers.Add(i);
+                numbers2.Add(i);
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                if (numbers[i] < 5)
+                {
+                    numbers2[i] = numbers[i] * 2;
+                }
+                else
+                {
+                    numbers2[i] = numbers[i] / 2;
+                }
+            }
+        }
+    }
+}
